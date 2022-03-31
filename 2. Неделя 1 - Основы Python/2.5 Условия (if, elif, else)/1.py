5d6b80818488f96fd07f5a0dc19e1808b29a1d3e
RAX3R <45417916+vqpv@users.noreply.github.com>
2022-03-31 15:53:00 -0400

    2.5 - 1

---
+++ 2. Неделя 1 - Основы Python/2.5 Условия (if, elif, else)/1.py
@@ -0,0 +1,16 @@
+num = int(input())
+
+if num == 1:
+    print('I')
+elif num == 5:
+    print('V')
+elif num == 10:
+    print('X')
+elif num == 50:
+    print('L')
+elif num == 100:
+    print('C')
+elif num == 500:
+    print('D')
+elif num == 1000:
+    print('M')
