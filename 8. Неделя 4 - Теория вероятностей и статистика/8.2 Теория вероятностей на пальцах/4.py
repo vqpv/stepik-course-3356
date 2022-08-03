098bc34763d5fa7e672b56f882123f555ec2a65d
RAX3R <45417916+vqpv@users.noreply.github.com>
2022-08-04 00:01:41 +0300

    8.2 - 4

---
+++ 8. Неделя 4 - Теория вероятностей и статистика/8.2 Теория вероятностей на пальцах/4.py
@@ -0,0 +1,12 @@
+import numpy
+
+n, m, k = int(input()), int(input()), int(input())
+
+c = 100000
+P = 0
+
+for _ in range(c):
+    t = sum(numpy.random.randint(1, m + 1, size=n))
+    P += int(t == k)
+
+print(round((P * 100) / c, 1), end='%')
